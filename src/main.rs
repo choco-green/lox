cc48b81bb090615d6723fe016eed7e2397eeb637
choco_green <choco_green@hotmail.com>
2024-02-03 18:54:27 +0000

    feat: Introducing the Interpreter Framework
    This Interpreter Framework is currently incomplete, as the interpreter is yet able to run the code provided by the user, but echos the input back to the user.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
-fn main() {
-    println!("Hello, world!")
+use std::{
+    env,
+    fs::read_to_string,
+    io::{self, stdin, stdout, Write},
+    path::Path,
+    process::exit,
+};
+
+fn main() -> io::Result<()> {
+    // Collect the command line arguments
+    let args = env::args().collect::<Vec<String>>();
+
+    // Match the number of arguments
+    // Argument of length 1 is omitted because it is the name of the program
+    match args.len() {
+        2 => {
+            if args[1] == "jlox" {
+                run_prompt()?;
+            }
+        }
+        3 => run_file(Path::new(&args[1]))?,
+        _ => {
+            eprintln!("Usage: jlox [script]");
+            exit(64);
+        }
+    }
+    Ok(())
+}
+
+/// Creates a new instance of the REPL and runs it
+fn run_prompt() -> io::Result<()> {
+    loop {
+        // Prompts user for the next command
+        print!("> ");
+        stdout().flush()?;
+
+        // Reads the input from the user
+        let mut input = String::new();
+        stdin().read_line(&mut input)?;
+
+        // If the input is empty, break the loop
+        if input.trim().is_empty() {
+            break;
+        }
+
+        run(input);
+    }
+
+    Ok(())
+}
+
+/// Reads the source code from the file and runs it
+fn run_file(file: &Path) -> io::Result<()> {
+    let bytes = read_to_string(file)?;
+    run(bytes);
+
+    Ok(())
+}
+
+/// Prints the tokens from the source code by splitting the source code by
+/// whitespace
+fn run(source: String) {
+    // Split the source code by whitespace
+    let token = source.split_whitespace().collect::<Vec<&str>>();
+
+    // Print the tokens, for now
+    for t in token {
+        println!("{}", t);
+    }
 }
